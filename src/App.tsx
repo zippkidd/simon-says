06b924c21edb13ea4317f749d982c57060224847
zippkidd <thomastylek@gmail.com>
2024-02-04 16:45:10 -0500

    some initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type FormSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => void
 
+interface Button {
+  color: string
+}
+
+const App = () => {
+  const handleSubmit: FormSubmitHandler = e => {
+    e.preventDefault();
+    console.log('onSubmit')
+  }
+
+  const [ buttons, setButtons ] = useState<Button[]>([
+    {color: 'red'},
+    {color: 'blue'},
+    {color: 'yellow'},
+    {color: 'green'}
+  ])
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simon Says</h1>
+      <section>
+        <h2>Game Settings</h2>
+        <form onSubmit = { handleSubmit }>
+          <label htmlFor="numberOfButtons">Number of Buttons</label>
+          <input type="number" id="numberOfButtons" min="0" max="100" />
+          <button type="submit">Update Settings</button>
+        </form>
+      </section>
+      <section>
+        <h2>Game</h2>
+        <button>Start Game</button>
+        <div aria-label="Game Board">
+          {
+            buttons.map((button, i) => {
+              return (<button style={{backgroundColor: button.color}}><span className="visually-hidden">{i + 1}</span></button>)
+            })
+          }
+        </div>
+      </section>
+      <section>
+        <h2>Score</h2>
+        <div className="score">0</div>
+      </section>
     </>
   )
 }
---
+++ src/Button.tsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+
+function Button(props) {
+  return (
+    <div>Button</div>
+  )
+}
+
+Button.propTypes = {
+  
+}
+
+export default Button
